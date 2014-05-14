41299b1124c3dda36e04a93fcccbc158f67e9f68
sovanseng <sovanseng@yahoo.com>
2014-05-14 14:37:32 -0400

    Update age.c

--- secE/age.c
+++ secE/age.c
@@ -3,13 +3,19 @@ int main(){
 
 	const int age;
 	int _age;
-	int 3age;//WRONG
+	//int 3age;//WRONG
 	int age3;
-	int main;//WRONG
+	//int main;//WRONG
+	
+	printf("Please enter your age\t :");
+	scanf("%d",&_age);	
+	printf("Please enter your friend's age :");
+	scanf("%d",&age3);
+	
+	printf("You are %d years old \n
+	
+		Your friend is %d years old \n", _age, age3);
 	
 	
-	
-
-
 	return 0;
 }
