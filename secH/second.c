bf9db664b387d5e0681fc6bc3db5c473a1c56da3
Olivier St-Cyr <olivier.st-cyr@matrix.senecac.on.ca>
2014-05-14 11:08:49 -0400

    Weeks 1 and 2, Olivier

---
+++ secH/second.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int var1;     // declare the variable var1 as type int
+	float var2;   // declare the variable var2 as type float
+
+	var1 = 20;    // assigning the value 20 to the variable var1
+	var2 = 6.9;   // assigning the value 6.9 to the variable var2
+
+
+	int var3 = 9; // declare the value var3 as type int and assign value 9 to it	
+
+	char firstInitial = 'O';
+
+	printf("Hello World!\n");
+
+	printf("My score is %d%%\n", var1);
+
+	printf("The values of var1 and var3 are: %d and %d\n", var1, var3);
+
+	printf("The value of var1 is: %d\nThe value of var2 is %f\n", var1, var2);
+
+	printf("My first initial is: %c\n", firstInitial);
+
+
+	return 0;
+}
---
+++ secH/third.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int myAge;
+
+	printf("Enter your age: ");
+
+	scanf("%d", &myAge);
+
+	printf("My age is: %d\n", myAge);
+
+	return 0;
+}
