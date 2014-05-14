73d3fb8796bd3f5691f993d160598174518ec36e
Catherine Leung <catherine.leung@senecacollege.ca>
2014-05-14 13:26:50 -0400

    fixed area.c

--- secF/area.c
+++ secF/area.c
@@ -5,17 +5,29 @@ triangle in centimeters(both are floating point values).  It will
 
 int main(void){
     float base;
-    flaot height;
+    float height;
     float area;
    
     printf("please enter the base: ");
     scanf("%f",&base);
     printf("please enter the height: ");
-    scanf("%f,&height)
-    
-    area = 1 / 2 * base * height;
+    scanf("%f",&height);
+    /*this expression is wrong because 1/2 is int/int so it is 0
+    area = 1 / 2 * (base * height); */
+    /*working: 
+    area = 1.0/2 * base* height;
+    */
+    /*
+    area = 0.5 * base * height;
+    */
+    area = (base*height)/2;
+
+
+
+
+
 
     printf("the area of the triangle with base: %f height: %f is %f\n",
                                                          base,height,area); 
     return 0;
-}}
+}
